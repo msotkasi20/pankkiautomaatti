98914a5737cdd6905c23b90b10dd0ec64a0b51a7
j3shi <jebuyt@gmail.com>
2025-01-21 23:56:45 +0200

    CRUD finished for card

--- backend/models/cardModel.js
+++ backend/models/cardModel.js
@@ -19,3 +19,38 @@ export async function getCardById(pool, id) {
         throw new Error(`Database error: ${error.message}`);
     }
 }
+
+export async function createCard(pool, card){
+    try{
+        const [rows] = await pool.query('INSERT INTO card (idcard, type, cardpin) VALUES (?, ?, ?)', [card.idcard, card.type, card.cardpin]);
+        return rows; // Palauttaa luodun kortin
+    } catch (error){
+        throw new Error(`Database error: ${error.message}`);
+    }
+}
+
+export async function updateCard(pool, id, card){
+    try {
+        const [rows] = await pool.query('UPDATE card SET type = ?, cardpin = ? WHERE idcard = ?', [card.type, card.cardpin, id]);
+        if (rows.length == 0) {
+            throw new Error('Card not found');
+
+        }
+        return rows[0];
+    } catch (error){
+        throw new Error(`Database error: ${error.message}`);
+    }
+}
+
+export async function deleteCard(pool, id){
+    try{
+        const [rows] = await pool.query('DELETE FROM card WHERE idcard = ?', [id]);
+        if (rows.length == 0) {
+            throw new Error('Card not found');
+
+        }
+        return rows[0];
+    } catch (error){
+        throw new Error(`Database error: ${error.message}`);
+    }
+}
